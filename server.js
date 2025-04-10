4fe758c297dc59005ccd64877d62d7c909c35549
BartekArseniuk <bartekarseniuk1701@gmail.com>
2025-04-10 12:28:44 +0200

    Update CORS configuration to allow requests from any origin

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
